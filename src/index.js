2e754bca3f6b96359b9c8c75a8a29a877da1bd25
Rob Evans <coolbloke1324@gmail.com>
2018-03-02 18:58:28 +0000

    Early exist if offset not defined

--- src/index.js
+++ src/index.js
@@ -398,6 +398,10 @@ export default class extends Component {
     const step = dir === 'x' ? state.width : state.height
     let loopJump = false
 
+    if (offset === undefined || this.internals.offset === undefined) {
+      return;
+    }
+    
     // Do nothing if offset no change.
     if (!diff) return
 
